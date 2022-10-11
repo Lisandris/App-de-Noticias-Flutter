1027edbe1db49936f2ecac4aafdc740916d8aa26
Lisandris <lis170694@gmail.com>
2022-10-11 19:24:29 +0200

    Implementado Provider como manejador de estado

--- lib/src/pages/tabs_pages.dart
+++ lib/src/pages/tabs_pages.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class TabsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
-   );
+    return ChangeNotifierProvider(
+      create: (_) => new _NavegacionModel(),
+      child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+       ),
+    );
   }
 }
 
@@ -16,8 +20,15 @@ class _Navegacion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel >(context);
+
+
+
+
     return BottomNavigationBar(
-      currentIndex: 0,
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (i) => navegacionModel.paginaActual =i,
       items:[
         BottomNavigationBarItem( icon: Icon( Icons.person_outline ), label: 'Para ti' ),
         BottomNavigationBarItem( icon: Icon( Icons.public ), label: 'Encabezados' ),
@@ -44,7 +55,19 @@ class _Paginas extends StatelessWidget {
           color: Colors.blue,
         ),
       ],
-
     );
+  }
+}
+
+
+class _NavegacionModel with ChangeNotifier{
+
+  int _paginaActual = 0;
+
+  int get paginaActual => this._paginaActual;
+
+  set paginaActual( int valor ){
+    this._paginaActual = valor;
+    notifyListeners();
   }
 }
