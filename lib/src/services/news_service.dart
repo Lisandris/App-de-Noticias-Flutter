84394ce49301d73fac486873df01483b91b3368d
Lisandris <lis170694@gmail.com>
2022-10-11 20:14:55 +0200

    NewsAPI Obtener noticias del mundo

---
+++ lib/src/services/news_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/src/models/news_models.dart';
+
+
+class NewsService with ChangeNotifier{
+
+
+  List<Article> headlines = [];
+
+  NewsService(){
+
+
+    this.getTopHeadlines(); 
+  }
+
+
+  getTopHeadlines(){
+    print('cargando headlines..');
+  }
+}
